521591625e167b9889829e593f24df15540eb1d9
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-29 22:05:14 +0300

    completed Assignemt

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main') //select & remove element
+main.remove();
+
+const newHeader = document.createElement('h1');//create new Element
+newHeader.id = 'victory';                       //add id
+newHeader.innerHTML = "Miriam is the champion"; //add text
+
