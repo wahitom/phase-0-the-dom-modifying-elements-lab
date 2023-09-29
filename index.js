67cfd5c9874ff8b9ae3b978d23aa7f3051f3ff31
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-29 22:06:55 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const newHeader = document.createElement('h1');//create new Element
 newHeader.id = 'victory';                       //add id
 newHeader.innerHTML = "Miriam is the champion"; //add text
 
+document.body.appendChild(newHeader); //you have to append it to the doc for it to appeas
+
+
